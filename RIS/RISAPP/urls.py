4ec20c5d3d622e60eaf0e11510ec68c7de6f0c7d
dmanske001 <dmanske@unomaha.edu>
2024-05-07 20:19:29 -0500

    update

--- RIS/RISAPP/urls.py
+++ RIS/RISAPP/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('makerulechangerequest/', views.make_rule_change, name='makerulechangerequest'),
 ]
--- RIS/RISAPP/views.py
+++ RIS/RISAPP/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "RISAPP/index.html", {})
+
+def home(request):
+    return render(request, "RISAPP/home.html", {})
+
+def make_rule_change(request):
+    return render(request, "RISAPP/makerulechange.html", {})
+
