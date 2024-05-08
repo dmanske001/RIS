15cd483498338a0b12079b6a21b77219decc7d0b
dmanske001 <dmanske@unomaha.edu>
2024-05-08 16:46:52 -0500

    update
    update

--- RIS/RISAPP/models.py
+++ RIS/RISAPP/models.py
@@ -38,7 +38,7 @@ class Rule_Change(models.Model):
     rule = models.ForeignKey(Rule, on_delete=models.CASCADE, default=None, blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=None, blank=True, null=True)
     date_created = models.DateField(blank=False, auto_now_add=False, default=None, null=True)
-    date_decided = models.DateField(blank=False, auto_now_add=False, default=None, null=True)
+    date_decided = models.DateField(blank=True, auto_now_add=False, default=None, null=True)
     rule_description = models.CharField(max_length=200, default=None, null=True)
     rule_status = models.CharField(max_length=8, choices=STATUSES, default='pending', null=True)
 
