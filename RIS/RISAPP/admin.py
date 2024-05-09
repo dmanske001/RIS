a14136e9788b7659da224be5659a5e11a0244709
UNOmislas <mislas@unomaha.edu>
2024-05-09 16:05:42 -0500

    Admin Log Entries

--- RIS/RISAPP/admin.py
+++ RIS/RISAPP/admin.py
@@ -1,7 +1,75 @@
 from django.contrib import admin
 from .models import Category, Rule, Rule_Change
 
+from django.contrib.admin.models import LogEntry, DELETION
+from django.utils.html import escape
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('rule_title', 'status', 'category', 'created_at', 'updated_at')
+    list_filter = ('status',)
+    search_fields = ('rule_title', 'content',)
+
+
+def comment_count(obj):
+    return obj.commentmodel_set.count()
+
+
+comment_count.short_description = 'Comment count'
+
+
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    date_hierarchy = 'action_time'
+
+    list_filter = [
+        'user',
+        'content_type',
+        'action_flag'
+    ]
+
+    search_fields = [
+        'object_repr',
+        'change_message'
+    ]
+
+    list_display = [
+        'action_time',
+        'user',
+        'content_type',
+        'object_link',
+        'action_flag',
+    ]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_view_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def object_link(self, obj):
+        if obj.action_flag == DELETION:
+            link = escape(obj.object_repr)
+        else:
+            ct = obj.content_type
+            link = '<a href="%s">%s</a>' % (
+                reverse('admin:%s_%s_change' % (ct.app_label, ct.model), args=[obj.object_id]),
+                escape(obj.object_repr),
+            )
+        return mark_safe(link)
+
+    object_link.admin_order_field = "object_repr"
+    object_link.short_description = "object"
+
+
 admin.site.register(Category)
 admin.site.register(Rule)
 admin.site.register(Rule_Change)
-
